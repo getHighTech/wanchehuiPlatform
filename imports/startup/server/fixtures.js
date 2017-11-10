0a4140c16b9d139501b59c6e6700f7e3f835c0ff
Your Name <xsqfeather@gmail.com>
2017-11-10 13:10:52 +0800

    delete fix script

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -6,15 +6,5 @@ import { BalanceIncomes } from '/imports/api/balances/balance_incomes.js';
 
 Meteor.startup(() => {
   prebuildAdmin();
-  newUserId =   Accounts.createUser({
-        username: "13810861287",
-        password: "123456",
-      });
-  Meteor.users.update(newUserId, {
-    $set:{
-      profile: {
-        mobile: "13810861287"
-      }
-    }
-  })
+
 });
