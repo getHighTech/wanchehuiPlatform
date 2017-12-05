35530f71b9a6812a75dc15365322458050aec980
Your Name <you@example.com>
2017-12-05 17:52:17 +0800

    。。

--- imports/ui/components/pages/Withdrawals.jsx
+++ imports/ui/components/pages/Withdrawals.jsx
@@ -1,7 +1,8 @@
 'use strict';
 
 import React from "react";
-
+import Table from 'antd/lib/table';
+import "antd/lib/table/style";
 import { connect } from 'react-redux';
 
 class Withdrawals extends React.Component{
@@ -9,14 +10,53 @@ class Withdrawals extends React.Component{
     super(props);
 
   }
-
+state= {
+  balanceChargesData:[],
+  loadingTip:"加载中...",
+}
 
 
 
 
   render() {
+    const ShopColumns = [
+      {
+        title: '提现记录id',
+        dataIndex: '_id',
+        key: '_id',
+        width: 150,
+      },
+      {
+      title: '金额',
+      dataIndex: 'money',
+      key: 'money',
+      width: 150,
+    }, {
+      title: '银行卡号',
+      dataIndex: 'bankId',
+      key: 'bankId',
+      width: 150,
+    }, {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+      width: 150,
+    },
+    {
+      title: '时间',
+      dataIndex: 'creatAt',
+      key: 'creatAt',
+      width: 150,
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      key: 'status',
+      width: 150,
+    },
+  ];
     return (
-      <div><h1>提现管理</h1>开发中....</div>
+      <div>  <Table  columns={ShopColumns} /></div>
     )
   }
 }
