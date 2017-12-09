9e522603d014af0b1d15172f3804936592f53cc2
simontaosim <xsqfeather@gmail.com>
2017-12-09 15:22:38 +0800

    调整saga以及包的结构

---
+++ imports/http/balances.js
@@ -0,0 +1,26 @@
+import { Meteor } from 'meteor/meteor';
+
+import { getIncomeRecords } from '../api/balances/balance_incomes_actions.js'
+
+
+HTTP.methods({
+   '/api/v1/balances/:id/:userId/incomes/:page/:pagesize': {
+     get: function() {
+          let balanceId = this.query.id;
+          let userId = this.query.userId;
+          let page = this.query.page;
+          let pagesize = this.query.pagesize;
+         return getIncomeRecords(page, pagesize, balanceId, userId);
+     }
+   },
+   '/api/v1/balances/:id/charges':{
+     get: function(){
+       let mobile = this.query.mobile;
+       if (!mobile) {
+         return "MOBILE CANT BE BLANK"
+       }
+       return updateUsernameByMobile(mobile);
+
+     }
+   }
+ });
