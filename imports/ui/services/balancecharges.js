589e07c3d7edea97f34c5cbdbd4a1c5fccc7ead9
Your Name <you@example.com>
2017-12-22 15:01:06 +0800

    完成订单提现管理

--- imports/ui/services/balancecharges.js
+++ imports/ui/services/balancecharges.js
@@ -15,53 +15,23 @@ export function getMeteorBalanceCharge(condition,page,pageSize,callback){
       result.push(charge);
       let userId = charge.userId;
       bankIds.push(userId);
-      console.log(bankIds);
     }
 
-
-
-
-
-
-    // Meteor.call('orders.accouts',bankIds,function(err,alt){
-    //   Hash = {}
-    //   console.log('111');
-    //   for(let accout of alt) {
-    //     Hash[accout.userId] = accout;
-    //   }
-    //   for(var charge of result) {
-    //     charge.name = Hash[charge.userId].name;
-    //     console.log(result);
-    // }
-    // })
-    // Meteor.call("orders.accouts", bankIds, function(error, accouts) {
-    //   console.log(accouts);
-    //   if (!error) {
-    //     accoutHash = {}
-    //     for(let accout of accouts) {
-    //       accoutHash[accout.createdBy] = accout;
-    //       console.log(accout);
-    //     }
-    //     console.log(accoutHash)
-    //     for(var charge of result) {
-    //       console.log(result);
-    //       charge.name = accoutHash[charge.createdBy].name;
-    //   }
-    //
-    //
-    //   }
-    // });
-
-
-
-
-
-
-
-
-
-
-
+    Meteor.call("orders.accouts", bankIds, function(error, accouts) {
+      console.log(accouts);
+      if (!error) {
+        accoutHash = {}
+        for(let accout of accouts) {
+          accoutHash[accout.createdBy] = accout;
+        }
+        console.log(accoutHash);
+        for(var charge of result) {
+          console.log(result);
+          console.log(accoutHash[charge.userId].name);
+          charge.name = accoutHash[charge.userId].name;
+      }
+      }
+    });
 
     Meteor.call("bankcards.accouts", bankIds, function(error, accouts) {
       console.log(accouts);
@@ -72,7 +42,6 @@ export function getMeteorBalanceCharge(condition,page,pageSize,callback){
         }
         console.log(accoutHash)
         for(var charge of result) {
-          console.log(charge.userId);
           charge.bankId = accoutHash[charge.userId].accountNumber;
           charge.address= accoutHash[charge.userId].bankAddress;
           charge.userId =  accoutHash[charge.userId].realName;
