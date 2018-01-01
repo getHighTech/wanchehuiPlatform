737234aff932e3c8158b29c9b0f590b3e41b0a55
simontaosim <xsqfeather@gmail.com>
2018-01-01 22:14:27 +0800

    准备处理不存在的代理节点

--- imports/api/agencies/checkAgencies.js
+++ imports/api/agencies/checkAgencies.js
@@ -1,5 +1,6 @@
 import { Agencies } from "./agencies.js";
 import { BalanceIncomes } from '../balances/balance_incomes.js';
+import { Balances } from '../balances/balances.js';
 export function checkAgencies(){
 
   let undeal = 0;
@@ -7,6 +8,8 @@ export function checkAgencies(){
   let needToAnyalse = 0;
   let thereIsNoSuper = 0;
   let dealingAgency = 0;
+  let illagency = 0;
+  let userNotFound = 0;
   Agencies.find({}).forEach((agency)=> {
     let income =BalanceIncomes.find({agency: agency._id});
     let superIncome = BalanceIncomes.findOne({agency: agency.superAgencyId});
@@ -22,12 +25,44 @@ export function checkAgencies(){
       console.log(`----------------------------------------`);
 
       console.log("此分享没有入账，有错误！！！");
+      let user = Meteor.users.findOne({_id: agency.userId});
+      if (user) {
+        console.log("这个代理的用户是", user.username);
+        if (user.cards) {
+          console.log("这个代理的用户有卡数量", user.cards.length);
+          if (user.cards.length >= 1) {
+            console.log('这个代理是合法的');
+
+          }else{
+            console.log('这个代理是非法的，应该被删除');
+
+            if (Agencies.remove({_id: agency._id})) {
+                console.log('删除成功');
+            }
+            illagency++;
+          }
+        }else{
+          console.log('这个代理是非法的，应该被删除');
+          if (Agencies.remove({_id: agency._id})) {
+              console.log('删除成功');
+          }
+          illagency++;
+        }
+
+      }else{
+        console.log('此代理的用户不存在！');
+        Agencies.remove({_id: agency._id});
+        userNotFound++;
+      }
+
       undeal++;
       if (superAgency) {
         console.log("上级存在");
         let superUser = Meteor.users.findOne({_id: superAgency.userId});
         let superSuperAgency = Agencies.findOne({_id: superAgency.superAgencyId})
         console.log("上级代理用户是： ", superUser.username);
+        let balance = Balances.findOne({userId: superAgency.userId});
+        console.log('应该入账的账户是', balance);
 
         if (!superSuperAgency) {
           console.log('上上级代理不存在');
@@ -44,8 +79,10 @@ export function checkAgencies(){
           needToAnyalse++;
           if (superUser.username === "wanchehui") {
             console.log('上级代理是万车汇，万车汇代理为分销测试，可以不严格对应收入(万车汇拿不到佣金没有关系)');
-          }else{
-            console.log("上级没有账本，需要进一步分析"+needToAnyalse.toString());
+          }
+          if (superUser.username === "小马过河") {
+            console.log('上级代理是马哥，马哥作为初始级别用户，可以不严格对应收入');
+
           }
         }
       }else{
@@ -81,5 +118,7 @@ export function checkAgencies(){
   console.log("处理好的", correted);
   console.log("需要进一步分析的", needToAnyalse);
   console.log("没有上级的代理有", thereIsNoSuper);
+  console.log('非法的代理(没有买卡但是有代理权的人)', illagency);
+  console.log('用户不存在的代理有！', userNotFound);
     console.log(`========================================================`);
 }
