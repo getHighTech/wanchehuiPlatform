e94c06e158758df98cc098ec3e60e88382893e51
= <274173368@qq.com>
2018-01-15 17:26:49 +0800

    用户登陆&&忘记密码&&修改密码

--- imports/api/users/methods.js
+++ imports/api/users/methods.js
@@ -169,6 +169,50 @@ Meteor.methods({
   'user.findUsersbyUserIds'(userIds){
     return Meteor.users.find({_id: $in(userIds)})
   },
+  'users.update'(id,phone,address){
+   let user = Meteor.users.update(id,
+      {
+        $set: {
+          profile: {mobile: phone},
+          adderss: {location: address}
+        }
+      }
+    );
+     return user
+  },
+  'users.mobile.exist'(mobile){
+    let phone = Meteor.users.findOne(
+      {
+        'profile.mobile': mobile
+      }
+    )
+    if (!phone) {
+      return true
+    }else {
+      return false
+    }
+  },
+  'login.mobie'(mobile) {
+     let user = Meteor.users.findOne({'profile.mobile': mobile})
+     if(user == undefined){
+         user =  Accounts.createUser({username: mobile})
+         Meteor.users.update(user,{
+          $set: {
+            'profile.mobile': mobile
+          }
+         })
+     }
+     return user
+  },
+  'forgot.mobile'(mobile) {
+     let user = Meteor.users.findOne({'profile.mobile': mobile}) ||
+     Meteor.users.findOne({'username': mobile})
+     return  user._id
+  },
+  'set.password'(user,pwd) {
+      Accounts.setPassword(user,pwd,[])
+      return user
+  }
   // 'user.UserBindingRoles'(userId,roleIds){
   //   Meteor.users.update(userId, {
   //     $set: {
