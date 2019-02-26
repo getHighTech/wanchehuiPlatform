32a5ef19571aa9ac89404642c9fa7f99332f6e5e
simontaosim <xsqfeather@gmail.com>
2019-02-26 08:08:17 +0800

    删除不必要的系统配置

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -1,15 +1,18 @@
 module.exports = {
   servers: {
     one: {
+      // TODO: set host address, username, and authentication method
       host: '139.198.12.188',
       username: 'simon',
-      password: 'wanchehui112358'
+      password: 'xsq@519'
     }
   },
+
   app: {
     // TODO: change app name and path
     name: 'wanchehui',
     path: '../',
+
     servers: {
       one: {},
     },
@@ -21,23 +24,19 @@ module.exports = {
     env: {
       // TODO: Change to your app's url
       // If you are using ssl, it needs to start with https://
-      ROOT_URL: 'https://p.10000cars.cn',
-      MONGO_URL: "mongodb://wanchehui:Wanchehui112358@139.198.0.131:7017,139.198.0.131:7018,139.198.0.131:7019/wanchehui?replicaSet=foobar&authSource=wanchehui"
+      ROOT_URL: 'http://p.10000cars.cn',
+      MONGO_URL: 'mongodb://wanchehui:Wanchehui112358@139.198.0.131:7017,139.198.0.131:7018,139.198.0.131:7019/wanchehui?replicaSet=foobar&authSource=wanchehui',
     },
+
     docker: {
       // change to 'abernix/meteord:base' if your app is using Meteor 1.4 - 1.5
       image: 'abernix/meteord:node-8.4.0-base',
     },
-        // Show progress bar while uploading bundle to server
+
+    // Show progress bar while uploading bundle to server
     // You might need to disable it on CI servers
     enableUploadProgressBar: true,
-    deployCheckWaitTime: 1000
+    deployCheckWaitTime: 1000,
   },
-  // proxy: {
-  //   domains: 'p.10000cars.cn',
-  //   ssl: {
-  //     letsEncryptEmail: 'xsqfeather@126.com',
-  //   }
-  // },
 
-};
+}
